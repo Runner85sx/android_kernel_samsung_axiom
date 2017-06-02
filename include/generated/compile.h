d89d0291e9c70a031b0a19949a593fd1adbcda78
runner85sx <mathias.rittweger@gmx.de>
2017-06-02 04:42:42 +0200

    mrproper

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 2 */
-/* SMP PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 SMP PREEMPT Thu Mar 14 10:44:52 IST 2013"
-#define LINUX_COMPILE_BY "sandee.gupta"
-#define LINUX_COMPILE_HOST "sisoandroid14"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
